bc808b9d743067443bf961b0f5bcc588e3d5bf63
premakhil <akhilpchand@gmail.com>
2021-05-14 10:37:10 +0530

    Table created

--- run.py
+++ run.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, url_for, redirect, request
 
+import mysql.connector
+import yaml
+
+
+db = yaml.load(open('db.yaml'))
 app = Flask(__name__)
+
+
+mydb = mysql.connector.connect(
+    host=db["mysql_host"],
+    user=db["mysql_user"],
+    passwd=db["mysql_password"],
+    database=db["mysql_database"],
+)
+
+mycursor = mydb.cursor()
 
 
 @app.route('/')
@@ -10,7 +25,11 @@ def home():
 
 @app.route('/customers')
 def customers():
-    return render_template('customers.html')
+    mycursor.execute("SELECT * FROM customer_details;")
+
+    data = mycursor.fetchall()
+
+    return render_template('customers.html', data=data)
 
 
 if __name__ == '__main__':
